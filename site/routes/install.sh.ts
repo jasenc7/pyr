4e04cf5519fee1bce5942639e6cccb9cb8c518d4
Jasen Carroll <jasen@jasencarroll.com>
2026-04-17 08:59:19 -0400

    site: removed geo information on install logging

--- site/routes/install.sh.ts
+++ site/routes/install.sh.ts
@@ -1,14 +1,8 @@
 import { type HandlerFn } from "fresh";
 import SCRIPT from "../assets/install.sh";
 
-export const handler: HandlerFn<unknown, unknown> = (ctx) => {
-  const h = ctx.req.headers;
-  console.log(
-    JSON.stringify({
-      event: "install.sh",
-      country: h.get("cf-ipcountry"),
-    }),
-  );
+export const handler: HandlerFn<unknown, unknown> = () => {
+  console.log(JSON.stringify({ event: "install.sh" }));
   return new Response(SCRIPT, {
     headers: { "content-type": "application/x-sh" },
   });
